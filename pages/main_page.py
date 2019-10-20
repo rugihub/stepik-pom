a1ed20aabd4da37fd1a1f689dddc1e385c00c3c6
Ru Gi <rububit@gmail.com>
2019-10-20 09:00:42 +0300

    after 4.2.9

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,16 +8,14 @@ class MainPageLocators():
 class MainPage(BasePage): 
 
     def go_to_login_page(self):
-        #link = self.browser.find_element_by_css_selector("#login_link")
-        link = self.browser.find_element_by_id("login_link")
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-        link.click() 
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)        
         
     def should_be_login_link(self):
-        #assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-        
-        #assert self.is_element_present(By.ID, "login_link"), "Login link is present"
+        #assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not present"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not present"
+              
        
     
 
